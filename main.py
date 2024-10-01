4f6cff7d082be2aadd90bf0f821aebe385f58ba1
James-Heavey <james.heavey@ibm.com>
2024-10-01 13:31:34 +0100

    store encoded deata in pytorch

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
+import torch
+
 with open("shakespeare.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 print(text[:1000])
 
 # set of unique characters in the data set
-chars = sorted(list(set(text)))
-encoding_map = {char: i for i, char in enumerate(chars)}
-decoding_map = {i: char for i, char in enumerate(chars)}
+vocab = sorted(list(set(text)))
+encoding_map = {char: i for i, char in enumerate(vocab)}
+decoding_map = {i: char for i, char in enumerate(vocab)}
 
 
-def encoder(string: str, encoding_map: dict = encoding_map) -> list[int]:
+def encode(string: str, encoding_map: dict = encoding_map) -> list[int]:
     return [encoding_map[char] for char in string if char in encoding_map]
 
 
-def decoder(encoded_list: list[int], decoding_map: dict = decoding_map) -> str:
+def decode(encoded_list: list[int], decoding_map: dict = decoding_map) -> str:
     return "".join([decoding_map[i] for i in encoded_list if i in decoding_map])
 
 
-test_string = "First Citizen: Before we proceed any further, hear me speak."
-encoded = encoder(test_string)
-decoded = decoder(encoded)
+# encode the entire data set and store in a pytorch tensor
+data = torch.tensor(encode(text), dtype=torch.long)
 
-print(f"Original: {test_string}")
-print(f"Encoded: {encoded}")
-print(f"Decoded: {decoded}")
+print(data.shape, data.dtype)
 
-assert test_string == decoded, "The decoded string does not match the original"
-print("Test passed: The encoder and decoder work correctly.")
+print(data[:1000])
+
+
+# Split the data into train and validation sets
+train_size = int(0.9 * len(data))
+train_data = data[:train_size]
+val_data = data[train_size:]
+
+print(f"Train data shape: {train_data.shape}")
+print(f"Validation data shape: {val_data.shape}")
