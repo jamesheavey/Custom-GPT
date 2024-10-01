b5bad32a060e1562aa173754e2164870bd4dd4ff
James-Heavey <james.heavey@ibm.com>
2024-10-01 13:55:00 +0100

    add batching

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import torch
 with open("shakespeare.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
-print(text[:1000])
-
 # set of unique characters in the data set
 vocab = sorted(list(set(text)))
 encoding_map = {char: i for i, char in enumerate(vocab)}
@@ -22,15 +20,43 @@ def decode(encoded_list: list[int], decoding_map: dict = decoding_map) -> str:
 # encode the entire data set and store in a pytorch tensor
 data = torch.tensor(encode(text), dtype=torch.long)
 
-print(data.shape, data.dtype)
-
-print(data[:1000])
-
-
 # Split the data into train and validation sets
 train_size = int(0.9 * len(data))
 train_data = data[:train_size]
 val_data = data[train_size:]
 
-print(f"Train data shape: {train_data.shape}")
-print(f"Validation data shape: {val_data.shape}")
+# training the model using chunks of the training data
+# the chunk size is also the maximum context window for generation
+context_window = 8
+train_data[: context_window + 1]
+
+x = train_data[:context_window]
+y = train_data[1 : context_window + 1]
+
+# for every chunk of training data we have a cumulative training example to predict the sequential character
+for t in range(context_window):
+    context = x[: t + 1]
+    target = y[t]
+
+
+batch_size = 4  # how many independent sequences will we process in parallel?
+
+
+def get_batch(data):
+    # get a random set of 4 indices from the data of size context window
+    indices = torch.randint(len(data) - context_window, (batch_size,))
+
+    # at each index create, extract the encoded char seq of length context window and stack them
+    input_batch = torch.stack([data[i : i + context_window] for i in indices])
+    # do the same for the targets
+    target_batch = torch.stack([data[i + 1 : i + context_window + 1] for i in indices])
+    return input_batch, target_batch
+
+
+input_batch, target_batch = get_batch(train_data)
+
+for batch_index in range(batch_size):
+    for time_step in range(context_window):
+        context = input_batch[batch_index, : time_step + 1]
+        target = target_batch[batch_index, time_step]
+        print(f"when input is {context.tolist()} the target: {target}")
